efd61c4691a22c8919147d3a95742d2d2ef1d5f3
Chaitanya Bandiwdekar <c.bandiwdekar@somaiya.edu>
2021-10-22 14:00:56 +0530

    Dish List done

--- lib/dish_card.dart
+++ lib/dish_card.dart
@@ -1,17 +1,21 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'dish.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class DishCard extends StatelessWidget {
-  final Dish dish = Dish(
-      "Veg Manchurian",
-      300,
-      "Soft vegetable balls served with chinese gravy",
-      4.5,
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5c_yHX2htBSQmrKUo6h4bw5rctcQn4LRsLg&usqp=CAU",
-      true);
+  final Dish dish;
+  DishCard(this.dish);
+  // final Dish dish = Dish(
+  //     "Veg Manchurian",
+  //     300,
+  //     "Soft vegetable balls served with chinese gravy",
+  //     4.5,
+  //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5c_yHX2htBSQmrKUo6h4bw5rctcQn4LRsLg&usqp=CAU",
+  //     true);
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +23,7 @@ class DishCard extends StatelessWidget {
       height: 180,
       child: Card(
           color: Colors.white,
-          margin: EdgeInsets.all(15),
+          margin: EdgeInsets.fromLTRB(15, 7.5, 15, 7.5),
           child: Padding(
               padding: EdgeInsets.all(10),
               child: Row(
@@ -64,17 +68,26 @@ class DishCard extends StatelessWidget {
                           children: [
                             Text(
                               dish.name,
-                              style: TextStyle(fontSize: 19),
+                              style: TextStyle(
+                                  fontSize: 19, fontWeight: FontWeight.bold),
                             ),
                             // Expanded(
                             //   flex: 1, // default
                             //   child: SizedBox(), // required field
                             // ),
                             Spacer(),
-                            Icon(
-                              Icons.circle,
-                              color: dish.veg ? Colors.green : Colors.red,
-                              size: 15,
+                            Container(
+                              padding: const EdgeInsets.all(2),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: dish.veg
+                                          ? Colors.green
+                                          : Colors.red)),
+                              child: Icon(
+                                Icons.circle,
+                                color: dish.veg ? Colors.green : Colors.red,
+                                size: 15,
+                              ),
                             ),
                           ],
                         ),
@@ -83,17 +96,24 @@ class DishCard extends StatelessWidget {
                         ),
                         Text(
                           "₹ " + dish.price.toString(),
-                          style: TextStyle(fontSize: 15),
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.bold),
                         ),
                         SizedBox(
                           height: 5,
                         ),
-                        Text(
-                          dish.description,
-                          style: TextStyle(fontSize: 10),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 5,
-                          softWrap: false,
+                        Expanded(
+                          flex: 5,
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: Text(
+                              dish.description,
+                              style: TextStyle(fontSize: 12),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 5,
+                              softWrap: false,
+                            ),
+                          ),
                         ),
                         Spacer(),
                         Row(
@@ -102,10 +122,12 @@ class DishCard extends StatelessWidget {
                             Container(
                                 height: 30, //height of button
                                 width: 30, //width of button
+
                                 child: ElevatedButton(
                                     onPressed: () {},
                                     child: Icon(Icons.remove_sharp),
                                     style: ElevatedButton.styleFrom(
+                                      padding: EdgeInsets.all(0),
                                       primary: Colors
                                           .black87, //background color of button
                                       shape: RoundedRectangleBorder(
@@ -113,31 +135,40 @@ class DishCard extends StatelessWidget {
                                               BorderRadius.circular(5)),
                                     ))),
                             SizedBox(
-                              width: 5,
+                              width: 10,
                             ),
-                            Text(
-                              "0",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  backgroundColor: Colors.greenAccent[600]),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(5)),
+                              width: 30,
+                              height: 30,
+                              child: Center(
+                                child: Text(
+                                  "0",
+                                  style: TextStyle(
+                                      fontSize: 16, color: Colors.white),
+                                ),
+                              ),
                             ),
                             SizedBox(
-                              width: 5,
+                              width: 10,
                             ),
-                            SizedBox(
+                            Container(
                                 height: 30, //height of button
                                 width: 30, //width of button
+
                                 child: ElevatedButton(
                                     onPressed: () {},
                                     child: Icon(Icons.add),
                                     style: ElevatedButton.styleFrom(
+                                      padding: EdgeInsets.all(0),
                                       primary: Colors
                                           .black87, //background color of button
                                       shape: RoundedRectangleBorder(
-                                          //to set border radius to button
                                           borderRadius:
                                               BorderRadius.circular(5)),
-                                    )))
+                                    ))),
                           ],
                         )
                       ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:food_delivery/dish.dart';
 import 'package:food_delivery/dish_card.dart';
 import 'package:food_delivery/cuisines.dart';
 
@@ -19,6 +20,36 @@ class DishList extends StatefulWidget {
 }
 
 class _DishListState extends State<DishList> {
+  List<Dish> dishes = [
+    Dish(
+        "Veg Manchurian",
+        250,
+        "Soft vegetable balls served with chinese gravy",
+        4.5,
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5c_yHX2htBSQmrKUo6h4bw5rctcQn4LRsLg&usqp=CAU",
+        true),
+    Dish(
+        "Chicken Chilly",
+        350,
+        "Marinated chicken pieces deep fried and served with spicy chinese gravy",
+        4,
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5c_yHX2htBSQmrKUo6h4bw5rctcQn4LRsLg&usqp=CAU",
+        false),
+    Dish(
+        "Schezwan Noodles",
+        300,
+        "Boiled noodles sauted in a variety of sauces and served with half boiled egg",
+        3.75,
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5c_yHX2htBSQmrKUo6h4bw5rctcQn4LRsLg&usqp=CAU",
+        true),
+    Dish(
+        "Schezwan fried rice",
+        350,
+        "Vegetable rice fried in schezwan sauce and served with chutney",
+        4.75,
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5c_yHX2htBSQmrKUo6h4bw5rctcQn4LRsLg&usqp=CAU",
+        true),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +57,11 @@ class _DishListState extends State<DishList> {
           title: Text("RASOI"),
           centerTitle: true,
         ),
-        // body: DishCard());
-        body: CuisineCard());
+        body: Container(
+          child: ListView(
+            children: dishes.map((dish) => DishCard(dish)).toList(),
+          ),
+        ));
+    // body: CuisineCard());
   }
 }
